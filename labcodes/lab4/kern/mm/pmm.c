214b415b87a559e2ee3226973d5a2e922f9aed73
123malin <2660180796@qq.com>
2015-04-25 16:27:14 +0800

    Update pmm.c

--- labcodes/lab4/kern/mm/pmm.c
+++ labcodes/lab4/kern/mm/pmm.c
@@ -434,7 +434,7 @@ get_page(pde_t *pgdir, uintptr_t la, pte_t **ptep_store) {
 //note: PT is changed, so the TLB need to be invalidate 
 static inline void
 page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
-    /* LAB2 EXERCISE 3: YOUR CODE
+    /* LAB2 EXERCISE 3: 2012011281
      *
      * Please check if ptep is valid, and tlb must be manually updated if mapping is updated
      *
@@ -450,15 +450,16 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
      * DEFINEs:
      *   PTE_P           0x001                   // page table/directory entry flags bit : Present
      */
-#if 0
-    if (0) {                      //(1) check if this page table entry is present
-        struct Page *page = NULL; //(2) find corresponding page to pte
-                                  //(3) decrease page reference
-                                  //(4) and free this page when page reference reachs 0
-                                  //(5) clear second page table entry
-                                  //(6) flush tlb
-    }
-#endif
+
+
+	if(*ptep & PTE_P)            //(1) check if this page table entry is present
+	{
+		struct Page *page = pte2page(*ptep);//(2) find corresponding page to pte
+		if(page_ref_dec(page) == 0)         //(3) decrease page reference
+			free_page(page);                //(4) and free this page when page reference reachs 0
+		*ptep = 0;                          //(5) clear second page table entry
+		tlb_invalidate(pgdir,la);           //(6) flush tlb
+	} 
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
